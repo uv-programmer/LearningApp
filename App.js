f2b4f0987a7a7a584d3412d1e9bf58ae208f2a49
Vishnu-at-196395329624 <Vishnu-at-196395329624>
2020-08-27 20:31:43 +0530

    textfield,button and styling

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <View>
+   <View style = {{padding:50}}>
+     <TextInput placeholder='Goal Name' style = {{borderColor: 'black' , borderWidth: 1, padding: 5 }} />
+     <Button title='Add'/>
+   </View>
+   <View></View>
+   </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+ 
   },
 });
