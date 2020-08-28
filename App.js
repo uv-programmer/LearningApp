43e21b9ff63997cacc3073910d0f80a9950c9c27
Vishnu-at-196395329624 <Vishnu-at-196395329624>
2020-08-28 11:30:46 +0530

    learning flex box and basic layout

--- App.js
+++ App.js
@@ -4,9 +4,9 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-   <View>
-   <View style = {{padding:50}}>
-     <TextInput placeholder='Goal Name' style = {{borderColor: 'black' , borderWidth: 1, padding: 5 }} />
+   <View style={{padding:50}}>
+   <View style = {{flexDirection:'row', justifyContent:'space-between',alignItems:'center' }}>
+     <TextInput placeholder='Goal Name' style = {{borderColor: 'black' , borderWidth: 1, padding: 5, width: '80%' }} />
      <Button title='Add'/>
    </View>
    <View></View>
